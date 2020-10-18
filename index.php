8464bb36ce176e1e1c284a381c9626500f2c7523
houyem16 <59013528+houyem16@users.noreply.github.com>
2020-10-18 15:06:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+   <head>
+      <title>Stock d'images</title>
+   </head>
+   <body>
+ <?php
+    try{
+        $bdd = new PDO('mysql:host=test.chq2ilqc9hoa.us-east-1.rds.amazonaws.com;dbname=testaws;charset=utf8', 'admin', 'Houyem16051996');
+    }catch(Exception $e){
+        die('Erreur : ');
+    }
+
+    $stmt=$bdd->prepare('SELECT lien FROM images');
+    $stmt->execute();
+
+    foreach($stmt as $key=>$val){
+        echo '<img src="/images'.$val['images.lien'].'" alt="logo" width="800"
+     height="400" ></br>';
+    }
+
+?>
+ </body>
+</html>            
